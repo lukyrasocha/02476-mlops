4c2b6227f1ad0bb0102b079f79e11af124a11163
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-03 14:55:30 +0100

    toy script to predict on new data

--- mlops_02476/predict_model.py
+++ mlops_02476/predict_model.py
@@ -1,17 +1,28 @@
 import torch
+import click
+from data import load_dataset
 
+
+@click.command()
+@click.argument("model_checkpoint")
+@click.argument("data_name")
 def predict(
-    model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
-) -> None:
-    """Run prediction for a given model and dataloader.
-    
-    Args:
-        model: model to use for prediction
-        dataloader: dataloader with batches
-    
-    Returns
-        Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
+    model_checkpoint,
+    data_name,
+) -> torch.tensor:
 
-    """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+  print(
+      f"Loading model from {model_checkpoint} and making a prediction on data set {data_name}")
+
+  model = torch.load(f"{model_checkpoint}")
+  dataloader = load_dataset(batch_size=64, setname=data_name)
+
+  predictions = torch.cat([model(batch) for batch, targets in dataloader], 0)
+
+  print(predictions.shape)
+
+  return predictions
+
+
+if __name__ == "__main__":
+  predict()
