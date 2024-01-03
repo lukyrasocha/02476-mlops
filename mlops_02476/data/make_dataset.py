e48ccd6625c5709704d9ef09f3164eeed0462192
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-03 14:55:06 +0100

    toy preprocess

--- mlops_02476/data/make_dataset.py
+++ mlops_02476/data/make_dataset.py
@@ -1,3 +1,14 @@
+import torch
 if __name__ == '__main__':
-    # Get the data and process it
-    pass
+  # Get the data and process it
+  train_data = torch.load('data/raw/train_images_0.pt')
+  # Standardize the data to have mean 0 and variance 1
+  train_data = (train_data - train_data.mean()) / train_data.std()
+
+  # Save the processed data
+  torch.save(train_data, 'data/processed/train_images_0.pt')
+
+  print('Data processed.')
+  print("Mean is: ", train_data.mean())
+  print("Standard deviation is: ", train_data.std())
+  print("Shape of data is: ", train_data.shape)
