267207048fa175f36d5ca7ac199e5ba931dc341f
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-05 17:10:23 +0100

    refactor to pytorch lightning

--- mlops_02476/models/model.py
+++ mlops_02476/models/model.py
@@ -1,3 +1,6 @@
+from torch.optim import Adam
+from torch.nn import functional as F
+import pytorch_lightning as pl
 from torch import nn
 
 
@@ -20,3 +23,46 @@ class FNN(nn.Module):
     x = self.fc3(x)
     x = nn.functional.log_softmax(x, dim=1)
     return x
+
+
+##############################
+# REFACTOR TO LIGHTNING MODULE#
+##############################
+
+
+class LightningFNN(pl.LightningModule):
+  def __init__(self):
+    super().__init__()
+    self.fc1 = nn.Linear(784, 128)
+    self.fc2 = nn.Linear(128, 64)
+    self.fc3 = nn.Linear(64, 10)
+
+  def forward(self, x):
+    # define forward pass
+    x = x.view(x.shape[0], -1)
+    x = self.fc1(x)
+    x = nn.functional.relu(x)
+    x = self.fc2(x)
+    x = nn.functional.relu(x)
+    x = self.fc3(x)
+    x = nn.functional.log_softmax(x, dim=1)
+    return x
+
+  def training_step(self, batch, batch_idx):
+    # training step logic
+    x, y = batch
+    y_hat = self(x)
+    loss = F.cross_entropy(y_hat, y)
+    self.log('train_loss', loss)
+    return loss
+
+  def validation_step(self, batch, batch_idx):
+    # validation step logic
+    x, y = batch
+    y_hat = self(x)
+    loss = F.cross_entropy(y_hat, y)
+    self.log('val_loss', loss)
+
+  def configure_optimizers(self):
+    optimizer = Adam(self.parameters(), lr=1e-3)
+    return optimizer
--- mlops_02476/train_model.py
+++ mlops_02476/train_model.py
@@ -2,46 +2,85 @@ import click
 import torch
 from datetime import datetime
 import matplotlib.pyplot as plt
-from models import FNN
+from models import FNN, LightningFNN
 from data import load_dataset
 
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
 
-@click.command()
-@click.option("--lr", default=1e-3, help="learning rate to use for training")
-def train(lr):
+
+import wandb
+
+
+# @click.command()
+# @click.option("--lr", default=1e-3, help="learning rate to use for training")
+
+def train():
   print("Training day and night")
 
-  model = FNN()
-  # We're only using the training data here
-  train_loader = load_dataset(batch_size=64)
+  # wandb.init(project="corrupt_mnist_fnn", entity="lukyrasocha") #project is the name of the project, entity is the name of the team (for collaboration) or user e.g. lukyrasocha
+  # config is a dictionary of all the hyperparameters you want to track
+  # wandb.init(config={"lr": 1e-3}, project="corrupt_mnist_fnn", entity="lukyrasocha")
 
-  optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-  loss_fn = torch.nn.CrossEntropyLoss()
-  training_loss = []
+  # model = FNN()
 
-  for epoch in range(10):  # Number of epochs
-    for images, targets in train_loader:
-      # Flatten the images
-      images = images.view(images.size(0), -1)
+  wandb_logger = WandbLogger(project="LIGHTNING_TEST")
 
-      optimizer.zero_grad()
-      output = model(images)
-      loss = loss_fn(output, targets)
-      loss.backward()
-      optimizer.step()
-      training_loss.append(loss.item())
+  early_stopping = EarlyStopping(
+      monitor='val_loss',
+      min_delta=0.00,
+      patience=3,
+      verbose=True
+  )
+
+  trainer = Trainer(
+      logger=wandb_logger,
+      callbacks=[early_stopping],
+      max_epochs=100,
+      limit_train_batches=0.2
+  )
+
+  model = LightningFNN()
+
+  train_loader, val_loader = load_dataset(batch_size=64)
+
+  trainer.fit(model, train_loader, val_loader)
+
+  # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+  # optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config.lr)
+
+  # loss_fn = torch.nn.CrossEntropyLoss()
+  # training_loss = []
+
+  # for epoch in range(10):  # Number of epochs
+  #  for images, targets in train_loader:
+  #    # Flatten the images
+  #    images = images.view(images.size(0), -1)
+
+  #    optimizer.zero_grad()
+  #    output = model(images)
+  #    loss = loss_fn(output, targets)
+  #    loss.backward()
+  #    optimizer.step()
+  #    training_loss.append(loss.item())
+
+  #    wandb.log({"loss": loss.item()})  # Log the loss
 
   # Save model
 
-  time = datetime.now().strftime("%Y%m%d%H%M%S")
-  torch.save(model, f"models/model_{time}.pt")
+  # time = datetime.now().strftime("%Y%m%d%H%M%S")
+  # torch.save(model, f"models/model_{time}.pt")
 
-  plt.plot(training_loss)
-  plt.title("Training Loss")
-  plt.xlabel("Iterations")
-  plt.ylabel("Loss")
+  # plt.plot(training_loss)
+  # plt.title("Training Loss")
+  # plt.xlabel("Iterations")
+  # plt.ylabel("Loss")
   # Save figure to reports/figures
-  plt.savefig(f"reports/figures/loss_{time}.png")
+  # plt.savefig(f"reports/figures/loss_{time}.png")
+
+  # wandb.log({"Training Loss Plot": wandb.Image(plt)})
+  # wandb.finish()  # Finish the WandB run
 
 
 if __name__ == "__main__":
