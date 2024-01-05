7bbe91b59133bc2e810f0685be68c57dcc236526
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-05 17:09:10 +0100

    add val dataset

--- mlops_02476/data/load_dataset.py
+++ mlops_02476/data/load_dataset.py
@@ -5,17 +5,17 @@ from torch.utils.data import TensorDataset, DataLoader
 def load_dataset(batch_size=64, setname='0'):
   # Load the data
   train_images = torch.load(f'data/processed/train_images_{setname}.pt')
-  # test_images = torch.load('data/processed/test_images.pt')
+  test_images = torch.load('data/processed/train_images_6.pt')
 
   train_targets = torch.load(f'data/processed/train_target_{setname}.pt')
-  # test_targets = torch.load('data/corruptmnist/test_target.pt')
+  test_targets = torch.load('data/processed/train_target_6.pt')
 
   # Creating tensor datasets
   train_dataset = TensorDataset(train_images, train_targets)
-  # test_dataset = TensorDataset(test_images, test_targets)
+  test_dataset = TensorDataset(test_images, test_targets)
 
   # Creating data loaders
   train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-  # test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+  test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-  return train_loader  # , test_loader
+  return train_loader, test_loader
